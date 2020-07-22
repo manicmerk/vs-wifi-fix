37aff3f52ddaac33f439ac0ccd0432ec4d864e3f
pbemerk <pbemerk@gmail.com>
2020-07-22 08:27:36 -0400

    Initial commit for visual studio version of app

---
+++ vs-wifi-fix/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+
+namespace Wi_Fi_Drop_Fix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {// Initiate CMD prompt to display autoconfig setting causing lag bombs.
+                string destination = "/c netsh wlan show settings";
+                System.Diagnostics.Process.Start("CMD.exe", destination);
+                Console.ReadLine();
+
+                Console.WriteLine("\n Do you want to toggle AutoConfig setting for your Wi-Fi Adapter?");
+                Console.Write("\n Please input On, Off, Help or Quit? ");
+
+                string outcome = GetUserInput();
+
+                Console.WriteLine("\n " + outcome + "? Press ENTER to continue.");
+                Console.ReadLine();
+
+                string currentDirectory = Directory.GetCurrentDirectory();
+                DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+                var fileName = Path.Combine(directory.FullName, "MostRecentWifiLog.txt");
+                var file = new FileInfo(fileName);
+
+                var reader = new StreamReader(file.FullName);
+                Console.SetIn(reader);
+                string recentWiFi = Console.ReadLine();
+                Console.WriteLine("Your most recently used WiFi was " + recentWiFi + "? Would you like to use it again?");
+                Console.WriteLine("Yes or No?");
+                string useRecent = GetUserInput();
+
+                if (outcome == "ON" || outcome == "OFF")
+                {
+
+                    Console.Write("\n Enter your Wi-Fi adapter name here: ");
+                    string adapter = GetUserInput();
+
+                    if (outcome == "ON")
+                    {
+                        Console.WriteLine("You can now detect wireless routers.");
+                        ToggleOn(adapter);
+                        // Log event to log file.
+                    }
+                    else if (outcome == "OFF")
+                    {
+                        ToggleOff(adapter);
+                        Console.WriteLine("You are connected to wireless but unable to detect new routers.");
+                        // Log event to log file.
+                    }
+                }
+                else if (outcome == "QUIT")
+                {
+                    Console.WriteLine("\n Goodbye!");
+                    break;
+                }
+
+                // Better placement for log. Give timestamp and log outcome (option).
+
+                Console.ReadLine();
+            }
+        }
+
+        // Method to return answer for user outcome interface.
+        static public string GetUserInput()
+        {
+            string outcome = Console.ReadLine();
+            return outcome.ToUpper();
+        }
+
+        // Toggle Autoconfig setting on or off.
+        static void ToggleOn(string adapter)
+        {
+            string toggleOn = "/c netsh wlan set autoconfig enabled=yes interface=\"" + adapter + "\"";
+            Process.Start("CMD.exe", toggleOn);
+        }
+
+        static void ToggleOff(string adapter)
+        {
+            string toggleOff = "/c netsh wlan set autoconfig enabled=no interface=\"" + adapter + "\"";
+            Process.Start("CMD.exe", toggleOff);
+        }
+
+    }
+}
---
+++ vs-wifi-fix/ReadMe.md
@@ -0,0 +1,37 @@
+The NetGear A7000 Dropped Packed Fix (Also a general fix for dropped packets, or at least an option to see if it solves your problem if you're having buffering issues during gameplay, zoom meetings, etc.)
+
+This wifi adapter is supposed to be an answer to your problems of low wifi connectivity, but instead you're getting insane packet loss every 20-30 seconds you say? Your video conferences got you down with a frown when you're always playing catchup? Well the internet helped me find a fix, and now I'm trying to create a simple tool to automate an obnoxious process for a problem that shouldn't even exist!
+
+Basically... everytime your adapter is scanning for wifi access points, for no apparent reason, it drops a crap ton of packets pushing your ping through the roof! Well worry no more, because before you know it you'll be smooth surfing the web in no time flat. This little toggle should take all the pain out of your games, conferences and YouTube-explorations!
+
+Dependencies:
+
+DotNet Core 3.1 Runtime or SDK
+https://dotnet.microsoft.com/download
+
+IMPORTANT:
+
+Only works on Windows machines.
+
+Must run as adminstator to work.
+
+* You’ll want to toggle the setting OFF when connected and working on the internet, playing games, etc. If for some reason you need to connect to a different wifi connection you’ll need to toggle the setting ON to be discoverable.
+* On a restart of your computer you’ll need to toggle the setting ON to connect to your wifi and then back OFF. It’s inconvenient sure, but way better than having to run the commands yourself from cmd everytime :-)
+
+
+
+Credit to:
+
+This for being the place I found the fix.
+
+https://community.netgear.com/t5/WiFi-Adapters/Netgear-Nighthawk-A7000-massive-ping-increase-at-random/td-p/1549696
+
+
+
+And this for being the place that the people who posted it where I found it, found it.
+
+https://www.reddit.com/r/GlobalOffensive/comments/3ahg59/fix_for_wireless_ping_spikes/
+
+Stay Strong and Game On!
+
+-The Manic Merk
