0fed5ad3bb6ebddcdc04b9a32b0a9bfdeb7290ac
pbemerk <pbemerk@gmail.com>
2020-07-24 12:05:16 -0400

    Implemented capture and write to file of most recent wi-fi. Stuck on scope of adapter variable in if else statement.

--- vs-wifi-fix/Program.cs
+++ vs-wifi-fix/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Net;
 using System.Diagnostics;
+using System.ComponentModel.Design;
 
 namespace Wi_Fi_Drop_Fix
 {
@@ -16,7 +17,7 @@ namespace Wi_Fi_Drop_Fix
             while (true)
             {// Initiate CMD prompt to display autoconfig setting causing lag bombs.
                 string destination = "/c netsh wlan show settings";
-                System.Diagnostics.Process.Start("CMD.exe", destination);
+                Process.Start("CMD.exe", destination);
                 Console.ReadLine();
 
                 Console.WriteLine("\n Do you want to toggle AutoConfig setting for your Wi-Fi Adapter?");
@@ -27,23 +28,37 @@ namespace Wi_Fi_Drop_Fix
                 Console.WriteLine("\n " + outcome + "? Press ENTER to continue.");
                 Console.ReadLine();
 
-                string currentDirectory = Directory.GetCurrentDirectory();
-                DirectoryInfo directory = new DirectoryInfo(currentDirectory);
-                var fileName = Path.Combine(directory.FullName, "MostRecentWifiLog.txt");
-                var file = new FileInfo(fileName);
+                // Code I was attempting to use to read and write a file. Simplified solution was File.WriteAllText()/File.ReadAllText()
+                //string currentDirectory = Directory.GetCurrentDirectory();
+                //DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+                //var fileName = Path.Combine(directory.FullName, "MostRecentWifiLog.txt");
+                //var mostRecentWifi = ReadFile(fileName);
+                //Console.WriteLine(mostRecentWifi);
 
-                var reader = new StreamReader(file.FullName);
-                Console.SetIn(reader);
-                string recentWiFi = Console.ReadLine();
-                Console.WriteLine("Your most recently used WiFi was " + recentWiFi + "? Would you like to use it again?");
-                Console.WriteLine("Yes or No?");
-                string useRecent = GetUserInput();
+                //string mostRecentWifi = File.ReadAllText("MostRecentWifiLog.txt");
+                //Console.WriteLine(mostRecentWifi);
+
+
 
                 if (outcome == "ON" || outcome == "OFF")
                 {
+                    if (File.Exists("MostRecentWifiLog.txt"))
 
-                    Console.Write("\n Enter your Wi-Fi adapter name here: ");
-                    string adapter = GetUserInput();
+                    {
+                        var mostRecentWifi = File.ReadAllText("MostRecentWifiLog.txt");
+                        Console.WriteLine("\n Your most recently used WiFi was " + mostRecentWifi + "? Would you like to use it again?");
+                        Console.Write("\n Yes or No?: ");
+                        string useRecent = GetUserInput();
+                        Console.WriteLine("\n " + useRecent + "? Hit ENTER to confirm.");
+                        Console.ReadLine();
+                        string adapter = mostRecentWifi;
+                    }
+                    else
+                    {
+                        Console.Write("\n Enter your Wi-Fi adapter name here: ");
+                        string adapter = GetUserInput();
+                        File.WriteAllText("MostRecentWifiLog.txt", adapter);
+                    }                  
 
                     if (outcome == "ON")
                     {
@@ -57,11 +72,25 @@ namespace Wi_Fi_Drop_Fix
                         Console.WriteLine("You are connected to wireless but unable to detect new routers.");
                         // Log event to log file.
                     }
+                    
+
                 }
-                else if (outcome == "QUIT")
+
+                // Can't figure out a way to make the console open the picture in image viewer?? Non essential to completion, fix if time permits.
+                //else if (outcome == "HELP")
+                //{
+                //    string currentDirectory = Directory.GetCurrentDirectory();
+                //    DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+                //    string helpFile = Path.Combine(directory + "\\Capture.JPG");
+                //    Process.Start("CMD.exe", helpFile);
+
+                //    File.Open("capture.jpg", FileMode.Open, FileAccess.ReadWrite);
+                //}
+
+                else if (outcome == "QUIT") 
                 {
                     Console.WriteLine("\n Goodbye!");
-                    break;
+                    Environment.Exit(0);
                 }
 
                 // Better placement for log. Give timestamp and log outcome (option).
@@ -69,6 +98,15 @@ namespace Wi_Fi_Drop_Fix
                 Console.ReadLine();
             }
         }
+
+        // Method to read most recent wi-fi name. Not necessary due to File.WriteAllText()
+        //public static string ReadFile(string fileName)
+        //{
+        //    using(var reader = new StreamReader(fileName))
+        //    {
+        //        return reader.ReadLine();
+        //    }
+        //}
 
         // Method to return answer for user outcome interface.
         static public string GetUserInput()
